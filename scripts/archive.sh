f697f74644289b822a5d5558df6c659f7fa21f69
Luke Strange <92686634+luke-strange@users.noreply.github.com>
2025-06-05 10:52:04 +0100

    Update archive.sh
    Improve the script ready to set up using cronjob

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,6 +1,11 @@
-while true;
+#!/bin/bash
+v=0
+while [ $v -lt 2 ];
 do 
 curl "https://data.bus-data.dft.gov.uk/avl/download/gtfsrt" -s --create-dirs -o "${BODSARCHIVE}/gtfsrt/$(date +"%Y")/$(date +"%m")/$(date +"%d")/gtfsrt-$(date +"%Y%m%dT%H%M%S.zip")";
 curl "https://data.bus-data.dft.gov.uk/avl/download/bulk_archive" -s --create-dirs -o "${BODSARCHIVE}/sirivm/$(date +"%Y")/$(date +"%m")/$(date +"%d")/sirivm-$(date +"%Y%m%dT%H%M%S.zip")";
-sleep 30
+if [ $v -eq 0 ]; then
+    sleep 30
+fi
+((v++))
 done
