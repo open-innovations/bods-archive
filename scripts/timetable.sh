9237b934285c9e2e4203aa193b96bb6cabd6bfc5
Luke Strange <92686634+luke-strange@users.noreply.github.com>
2025-06-06 11:01:25 +0100

    Run with bash
    Bash supports arrays

--- scripts/timetable.sh
+++ scripts/timetable.sh
@@ -1,6 +1,5 @@
-
-regions=('yorkshire' 'west_midlands' 'wales' 'south_west' 'south_east' 'scotland' 'north_east' 'north_west' 'london' 'east_midlands' 'east_anglia')
-
+#!/bin/bash
+regions=("yorkshire" "west_midlands" "wales" "south_west" "south_east" "scotland" "north_east" "north_west" "london" "east_midlands" "east_anglia")
 for i in "${regions[@]}"; do
   url="https://data.bus-data.dft.gov.uk/timetable/download/gtfs-file/$i/"
   file="${BODSARCHIVE}/timetables/$(date +"%Y")/$(date +"%m")/$(date +"%d")/itm_"$i"_gtfs_$(date +"%Y%m%d").zip"
