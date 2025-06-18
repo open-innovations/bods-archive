090ae3c22ae46b032acafc5ff96972a91f2f3946
Luke Strange <92686634+luke-strange@users.noreply.github.com>
2025-06-18 17:12:26 +0100

    feat: utc date

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -2,8 +2,8 @@
 v=0
 while [ $v -lt 2 ];
 do 
-curl "https://data.bus-data.dft.gov.uk/avl/download/gtfsrt" -s --create-dirs -o "${BODSARCHIVE}/gtfsrt/$(date +"%Y")/$(date +"%m")/$(date +"%d")/gtfsrt-$(date +"%Y%m%dT%H%M%S.zip")";
-curl "https://data.bus-data.dft.gov.uk/avl/download/bulk_archive" -s --create-dirs -o "${BODSARCHIVE}/sirivm/$(date +"%Y")/$(date +"%m")/$(date +"%d")/sirivm-$(date +"%Y%m%dT%H%M%S.zip")";
+curl "https://data.bus-data.dft.gov.uk/avl/download/gtfsrt" -s --create-dirs -o "${BODSARCHIVE}/gtfsrt/$(TZ=UTC date +"%Y")/$(TZ=UTC date +"%m")/$(TZ=UTC date +"%d")/gtfsrt-$(TZ=UTC date +"%Y%m%dT%H%M%S.zip")";
+curl "https://data.bus-data.dft.gov.uk/avl/download/bulk_archive" -s --create-dirs -o "${BODSARCHIVE}/sirivm/$(TZ=UTC date +"%Y")/$(TZ=UTC date +"%m")/$(TZ=UTC date +"%d")/sirivm-$(TZ=UTC date +"%Y%m%dT%H%M%S.zip")";
 if [ $v -eq 0 ]; then
     sleep 30
 fi
