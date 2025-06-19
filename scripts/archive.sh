68bd78485c7a9de2689b46c4f3d2329fe41c3794
slowe <299787+slowe@users.noreply.github.com>
2025-06-19 10:07:05 +0100

    Set permissions and adjust sleep

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 v=0
+types=("gtfsrt" "sirivm")
+urls=("https://data.bus-data.dft.gov.uk/avl/download/gtfsrt" "https://data.bus-data.dft.gov.uk/avl/download/bulk_archive")
+length=${#types[@]}
 while [ $v -lt 2 ];
 do
-gtfsrt="${BODSARCHIVE}/gtfsrt/$(TZ=UTC date +"%Y")/$(TZ=UTC date +"%m")/$(TZ=UTC date +"%d")/gtfsrt-$(TZ=UTC date +"%Y%m%dT%H%M%S.zip")"
-sirivm="${BODSARCHIVE}/sirivm/$(TZ=UTC date +"%Y")/$(TZ=UTC date +"%m")/$(TZ=UTC date +"%d")/sirivm-$(TZ=UTC date +"%Y%m%dT%H%M%S.zip")"
-curl "https://data.bus-data.dft.gov.uk/avl/download/gtfsrt" -s --create-dirs -o $gtfsrt && chmod 0775 $gtfsrt;
-curl "https://data.bus-data.dft.gov.uk/avl/download/bulk_archive" -s --create-dirs -o $sirivm && chmod 0775 $sirivm;
-if [ $v -eq 0 ]; then
-    sleep 30
-fi
-((v++))
+	# Get start time
+	start=`date +%s`
+	# Build the date part of the directory string
+	dtdir="$(TZ=UTC date +"%Y/%m/%d")"
+	# Loop over the types
+	for (( j=0; j<${length}; j++ ));
+	do
+		dir="${BODSARCHIVE}/${types[$j]}/$dtdir"
+		if [ ! -d "$dir" ]; then
+			# Create directory and set permissions
+			mkdir -p $dir
+			chmod 755 $dir
+		fi
+		# Download file
+		curl "${urls[$j]}" -s --create-dirs -o "$dir/${types[$j]}-$(TZ=UTC date +"%Y%m%dT%H%M%S.zip")" && chmod 0755 "${_}";
+	done
+	# Get end time
+	end=`date +%s`
+	# Calculate the time to sleep for (30s - time taken)
+	wait=$((30-$((end-start))))
+	if [ $v -eq 0 ]; then
+		sleep $wait
+	fi
+	((v++))
 done
