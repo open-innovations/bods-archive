314ea1eb829b0c2d8f26c791ae18be6925de2458
luke-strange <92686634+luke-strange@users.noreply.github.com>
2025-07-30 17:20:30 +0100

    refactor: major speed improvements
    - implement timing
    - use enumerate
    - reload the feed object to improve speed
    - use .extend to append to list

--- scripts/python/gtfs_realtime_utils.py
+++ scripts/python/gtfs_realtime_utils.py
@@ -1,20 +1,28 @@
 import glob
 from zipfile import ZipFile
 from google.transit import gtfs_realtime_pb2
+import time
+
+
+def log_num_files_parsed(i, total, t1):
+    if i % 100 == 0:
+        print(f"Parsed {round(i*100 / total)}% of {total} files in {round(time.time() - t1)} seconds.")
 
 def get_gtfs_from_binaries(paths: list):
     entities = []
-    feed = gtfs_realtime_pb2.FeedMessage()
+    total = len(paths)
+    assert total > 0, "paths is empty."
+    t1 = time.time()
 
-    assert len(paths) > 0, "paths is empty."
-
-    for path in paths:
+    for i, path in enumerate(paths, 1):
         if path.endswith(".bin"):
             with open(path, 'rb') as f:
+                feed = gtfs_realtime_pb2.FeedMessage()
                 feed.ParseFromString(f.read())
-                entities = entities + [e for e in feed.entity]
+                entities.extend(feed.entity)
         else:
             print(f"{path} is not a binary file.")
+        log_num_files_parsed(i, total, t1)
     return entities
 
 def get_gtfs_entities_from_zips(paths:list, bin_file='gtfsrt.bin'):
@@ -29,21 +37,23 @@ def get_gtfs_entities_from_zips(paths:list, bin_file='gtfsrt.bin'):
         The name of the binary file you expect to find in each zip file.
     '''
     entities = []
-    feed = gtfs_realtime_pb2.FeedMessage()
-
-    assert len(paths) > 0, "paths is empty."
-
-    for path in paths:
+    total = len(paths)
+    assert total > 0, "paths is empty."
+    t1 = time.time()
+    
+    for i, path in enumerate(paths, 1):
         if path.endswith(".zip"):
             with ZipFile(path) as zf:
                 if bin_file in zf.namelist():
                     with zf.open(bin_file, 'r') as f:
+                        feed = gtfs_realtime_pb2.FeedMessage()
                         feed.ParseFromString(f.read())
-                        entities = entities + [e for e in feed.entity]
-                else: 
+                        entities.extend(feed.entity)
+                else:
                     print(f'{bin_file} is not contained within {path}. Skipping to next zip file.')
-        else: 
+        else:
             print(f"{path} is not a zip file.")
+        log_num_files_parsed(i, total, t1)
     return entities
 
 def get_gtfs_entities_from_directory(DIR: str):
